79acc52b3329a987a931684158025392a5f500e6
Vetle Ledaal <vetle.ledaal@gmail.com>
2017-02-16 03:36:52 +0100

    Seperate example and actual code, use class instead

---
+++ cloudflare.php
@@ -0,0 +1,93 @@
+<?php
+
+class CloudflareSolver {
+
+	private $html;
+	private $url;
+
+
+	function __construct($html, $url) {
+		$this->html = $html;
+		$this->url = parse_url($url);
+	}
+
+	function getTimeout() {
+		if(preg_match('/}, (\d+)\);/', $this->html, $matches) !== 1)
+			return false;
+		return $matches[1];
+	}
+
+	function getSolvedUrl() {
+		if(!$this->isValid())
+			return false;
+		
+		// Prepare query
+		$query = [];
+		
+		// Read values from challenge-form
+		if(preg_match('/name="jschl_vc" value="([^"]+)"/', $this->html, $matches) !== 1)
+			return false;
+		$query['jschl_vc'] = $matches[1];
+		if(preg_match('/name="pass" value="([^"]+)"/', $this->html, $matches) !== 1)
+			return false;
+		$query['pass'] = $matches[1];
+		
+		// Solve the challenge
+		$query['jschl_answer'] = $this->solveJsChallenge();
+		return $query['jschl_answer']
+			 ? $this->url['scheme'] . '://' . $this->url['host'] . '/cdn-cgi/l/chk_jschl?' . http_build_query($query)
+			 : false;
+	}
+	
+	function isValid() {
+		// Check if common strings occur
+		return strpos($this->html, '/cdn-cgi/l/chk_jschl') !== false
+			&& strpos($this->html, 'challenge-form') !== false
+			&& strpos($this->html, 'jschl_vc') !== false
+			&& strpos($this->html, 'jschl_answer') !== false;
+	}
+
+	function solveJsChallenge() {
+		// Find the initial value
+		if(preg_match('/{"\w+":([^}]+)};/', $this->html, $matches) !== 1)
+			return false;
+		$challenge = self::decodeJsInt($matches[1]);
+
+		// Find all instructions to apply
+		if(preg_match_all('/([+\-*])=([^;]+);/', $this->html, $matches, PREG_SET_ORDER) == 0)
+			return false;
+
+		foreach($matches as $match) {
+			// Decode each expression and apply instruction
+			$op = $match[1];
+			$number = self::decodeJsInt($match[2]);
+			if(!self::mathExec($challenge, $number, $op))
+				return false;
+		}
+
+		// Return challenge result and length of the domain
+		return $challenge + strlen($this->url['host']);
+	}
+
+	private static function decodeJsInt($jsInt) {
+		if(preg_match('/^\+\(\(?([^);]+)\)\+\(([^);]+)\)\)$/', $jsInt, $matches) === 1) {
+			// Got 2 sets seperates into tens and singles
+			return self::decodeJsInt($matches[1])*10 + self::decodeJsInt($matches[2]);
+		} else {
+			// Count the expressions that equals '1'
+			return substr_count($jsInt, '!![]') + substr_count($jsInt, '!+[]');
+		}
+	}
+
+	private static function mathExec(&$a, $b, $op) {
+		// Only +, - and * have been observed
+		switch($op) {
+			case '+': $a += $b; return true;
+			case '-': $a -= $b; return true;
+			case '*': $a *= $b; return true;
+			case '/': $a /= $b; return true;
+			case '%': $a %= $b; return true;
+		}
+		return false;
+	}
+}
---
+++ example.php
@@ -0,0 +1,29 @@
+<?php
+include 'cloudflare.php';
+
+$html = <<<HTML
+var s,t,o,p,b,r,e,a,k,i,n,g,f, SdgCLeG={"q":!+[]+!![]+!+[]+!![]+!![]+!![]};
+        t = document.createElement('div');
+        t.innerHTML="<a href='/'>x</a>";
+        t = t.firstChild.href;r = t.match(/https?:\/\//)[0];
+        t = t.substr(r.length); t = t.substr(0,t.length-1);
+        a = document.getElementById('jschl-answer');
+        f = document.getElementById('challenge-form');
+        ;SdgCLeG.q+=+((!+[]+!![]+!![]+[])+(+[]));SdgCLeG.q*=+((!+[]+!+[]+!![]+!+[]+[])+(+!![]));SdgCLeG.q-=+((!+[]+!![]+!![]+[])+(+!![]));SdgCLeG.q*=+((+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));SdgCLeG.q*=+((!+[]+!![]+!![]+!![]+[])+(+[]));a.value = parseInt(SdgCLeG.q, 10) + t.length; '; 121'
+        f.submit();
+      }, 4000);
+    }, false); 
+  <form id="challenge-form" action="/cdn-cgi/l/chk_jschl" method="get">
+    <input type="hidden" name="jschl_vc" value="1a79a4d60de6718e8e5b326e338ae533"/>
+    <input type="hidden" name="pass" value="1487112786.013-dEMzfIsOva"/>
+    <input type="hidden" id="jschl-answer" name="jschl_answer"/>
+  </form>
+HTML;
+
+$cf = new CloudflareSolver($html, 'http://example.com');
+
+echo 'Waiting for ' . $cf->getTimeout()/1000 . ' seconds...' . "\r\n";
+usleep($cf->getTimeout() * 1000);
+
+// TODO: Go to the URL
+echo $cf->getSolvedUrl();
